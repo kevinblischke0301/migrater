accfdfbcfb45de37a495104bfbb9280c2241035f
Kevin Blischke <kevin.blischke@otto.de>
2025-01-29 09:33:59 +0100

    refactored migration functionality into its own package

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/joho/godotenv"
 
 	"github.com/kevinblischke0301/migrater/internal/db"
 	"github.com/kevinblischke0301/migrater/internal/env"
+	"github.com/kevinblischke0301/migrater/internal/service"
 )
 
 func main() {
@@ -42,38 +40,7 @@ func main() {
 		log.Fatal(fmt.Sprintf("Error while connecting to database:\n%s", err))
 	}
 
-	entries, err := os.ReadDir(env.MigrationDir)
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Error while reading migrations directory:\n%s", err))
-	}
-
-	for _, entry := range entries {
-		info, err := entry.Info()
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Error while reading file informations:\n%s", err))
-		}
-
-		fileName := info.Name()
-		pathName := filepath.Join(env.MigrationDir, fileName)
-		file, err := os.Open(pathName)
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Error while opening file \"%s\":\n%s", fileName, err))
-		}
-
-		fileContent, err := io.ReadAll(file)
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Error while reading file \"%s\":\n%s", fileName, err))
-		}
-
-		queries := strings.SplitAfter(string(fileContent), ";")
-		for i := 0; i < len(queries)-1; i++ {
-			_, err = db.Exec(queries[i])
-
-			if err != nil {
-				log.Fatal(fmt.Sprintf("Error while performing migration file \"%s\":\n%s", fileName, err))
-			}
-		}
-	}
+	service.Migrate(env.MigrationDir, db)
 
 	fmt.Println("Migration completed.")
 }
